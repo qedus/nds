3f349579135469f11c7cbc0da3981f15237d73ad
Jonathan Gillham <jonathan.gillham@gamil.com>
2014-04-30 09:42:15 +0100

    Used gofmt.

--- timeout.go
+++ timeout.go
@@ -1,8 +1,8 @@
 package nds
 
 import (
-    "appengine"
-    "time"
+	"appengine"
+	"time"
 )
 
 // Timeout returns a context that will set the timeout duration for datastore
@@ -11,12 +11,12 @@ import (
 // Use this packages Timeout function instead of appengine.Timeout to ensure
 // caching works with nds.
 func Timeout(c appengine.Context, d time.Duration) appengine.Context {
-    if ndsc, ok := c.(*context); ok {
-        return &context{
-            Context: appengine.Timeout(ndsc.Context, d),
-            RWMutex: ndsc.RWMutex,
-            cache: ndsc.cache,
-        }
-    }
-    return appengine.Timeout(c, d)
+	if ndsc, ok := c.(*context); ok {
+		return &context{
+			Context: appengine.Timeout(ndsc.Context, d),
+			RWMutex: ndsc.RWMutex,
+			cache:   ndsc.cache,
+		}
+	}
+	return appengine.Timeout(c, d)
 }
--- timeout_test.go
+++ timeout_test.go
@@ -1,52 +1,52 @@
 package nds_test
 
 import (
-    "github.com/qedus/nds"
-    "appengine/memcache"
-    "appengine/aetest"
-    "appengine/datastore"
-    "time"
-    "testing"
+	"appengine/aetest"
+	"appengine/datastore"
+	"appengine/memcache"
+	"github.com/qedus/nds"
+	"testing"
+	"time"
 )
 
 func TestTimeout(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    cc := nds.NewContext(c)
-    tc := nds.Timeout(cc, 10 * time.Second)
+	cc := nds.NewContext(c)
+	tc := nds.Timeout(cc, 10*time.Second)
 
-    key := datastore.NewIncompleteKey(cc, "TestEntity", nil)
-    type TestEntity struct {
-        Value int
-    }
-    key, err = nds.Put(tc, key, &TestEntity{52})
-    if err != nil {
-        t.Fatal(err)
-    }
+	key := datastore.NewIncompleteKey(cc, "TestEntity", nil)
+	type TestEntity struct {
+		Value int
+	}
+	key, err = nds.Put(tc, key, &TestEntity{52})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    if key.Incomplete() {
-        t.Fatal("incomplete key")
-    }
+	if key.Incomplete() {
+		t.Fatal("incomplete key")
+	}
 
-    // Create a new context to cause a memcache hit.
-    entity := &TestEntity{}
-    if err := nds.Get(nds.NewContext(c), key, entity); err != nil {
-        t.Fatal(err)
-    }
-    if entity.Value != 52 {
-        t.Fatal("incorrect entity.Value")
-    }
+	// Create a new context to cause a memcache hit.
+	entity := &TestEntity{}
+	if err := nds.Get(nds.NewContext(c), key, entity); err != nil {
+		t.Fatal(err)
+	}
+	if entity.Value != 52 {
+		t.Fatal("incorrect entity.Value")
+	}
 
-    // Check we used caching by seeing if there is the entity in cache.
-    stats, err := memcache.Stats(c)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if stats.Items == 0 {
-        t.Fatal("nothing in cache")
-    }
+	// Check we used caching by seeing if there is the entity in cache.
+	stats, err := memcache.Stats(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if stats.Items == 0 {
+		t.Fatal("nothing in cache")
+	}
 }
