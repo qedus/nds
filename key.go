099e6450db670d4970b782be10a7276c75abc342
Jonathan Gillham <jonathan.gillham@gamil.com>
2014-07-02 12:46:45 +0100

    Wrapped all Get, Put, Delete methods.

--- key.go
+++ key.go
@@ -1,70 +1,70 @@
 package nds
 
 import (
-    "appengine"
-    "appengine/datastore"
+	"appengine"
+	"appengine/datastore"
 )
 
 type Key struct {
-    datastore.Key
+	datastore.Key
 }
 
 func unwrapKeys(keys []*Key) []*datastore.Key {
-    datastoreKeys := make([]*datastore.Key, len(keys))
-    for i, k := range keys {
-        datastoreKeys[i] = &k.Key
-    }
-    return datastoreKeys
+	datastoreKeys := make([]*datastore.Key, len(keys))
+	for i, k := range keys {
+		datastoreKeys[i] = &k.Key
+	}
+	return datastoreKeys
 }
 
-func wrapKeys(keys []*datastore.Key) []*Key{
-    ndsKeys := make([]*Key, len(keys))
-    for i, k := range keys {
-        ndsKeys[i] = &Key{*k}
-    }
-    return ndsKeys
+func wrapKeys(keys []*datastore.Key) []*Key {
+	ndsKeys := make([]*Key, len(keys))
+	for i, k := range keys {
+		ndsKeys[i] = &Key{*k}
+	}
+	return ndsKeys
 }
 
 func NewIncompleteKey(c appengine.Context, kind string, parent *Key) *Key {
-    var parentKey *datastore.Key
-    if parent != nil {
-        parentKey = &parent.Key
-    }
-    return &Key{*datastore.NewIncompleteKey(c, kind, parentKey)}
+	var parentKey *datastore.Key
+	if parent != nil {
+		parentKey = &parent.Key
+	}
+	return &Key{*datastore.NewIncompleteKey(c, kind, parentKey)}
 }
 
 func NewKey(c appengine.Context,
-    kind, stringID string, intID int64, parent *Key) *Key {
-    var parentKey *datastore.Key
-    if parent != nil {
-        parentKey = &parent.Key
-    }
-    return &Key{*datastore.NewKey(c, kind, stringID, intID, parentKey)}
+	kind, stringID string, intID int64, parent *Key) *Key {
+	var parentKey *datastore.Key
+	if parent != nil {
+		parentKey = &parent.Key
+	}
+	return &Key{*datastore.NewKey(c, kind, stringID, intID, parentKey)}
 }
 
 func (k *Key) Parent() *Key {
-    return &Key{*k.Key.Parent()}
+	return &Key{*k.Key.Parent()}
 }
 
 func (k *Key) Equal(o *Key) bool {
-    return k.Key.Equal(&o.Key)
+	return k.Key.Equal(&o.Key)
 }
 
 func DecodeKey(encoded string) (*Key, error) {
-    key, err := datastore.DecodeKey(encoded)
-    if err != nil {
-        return nil, err
-    }
-    return &Key{*key}, err
+	key, err := datastore.DecodeKey(encoded)
+	if err != nil {
+		return nil, err
+	}
+	return &Key{*key}, err
 }
 
 func AllocateIDs(c appengine.Context,
-    kind string, parent *Key, n int) (low, high int64, err error) {
+	kind string, parent *Key, n int) (low, high int64, err error) {
 
-    var parentKey *datastore.Key
-    if parent != nil {
-        parentKey = &parent.Key
-    }
+	var parentKey *datastore.Key
+	if parent != nil {
+		parentKey = &parent.Key
+	}
 
-    return datastore.AllocateIDs(c, kind, parentKey, n)
+	return datastore.AllocateIDs(c, kind, parentKey, n)
 }
--- key_test.go
+++ key_test.go
@@ -1,134 +1,134 @@
-package nds
+package nds_test
 
 import (
-    "testing"
-    //"appengine/datastore"
-    "appengine/aetest"
+	"appengine/aetest"
+	"github.com/qedus/nds"
+	"testing"
 )
 
 func TestNewKeys(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    incompleteKey := NewIncompleteKey(c, "Test", nil)
-    if !incompleteKey.Incomplete() {
-        t.Fatal("expecting incomplete key")
-    }
+	incompleteKey := nds.NewIncompleteKey(c, "Test", nil)
+	if !incompleteKey.Incomplete() {
+		t.Fatal("expecting incomplete key")
+	}
 
-    key := NewKey(c, "Test", "string", 0, nil)
-    if key.Incomplete() {
-        t.Fatal("expected complete key")
-    }
+	key := nds.NewKey(c, "Test", "string", 0, nil)
+	if key.Incomplete() {
+		t.Fatal("expected complete key")
+	}
 }
 
 func TestAncestorKeys(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    parentKey := NewKey(c, "Parent", "", 1, nil)
-    incompleteKey := NewIncompleteKey(c, "Test", parentKey)
-    if !incompleteKey.Incomplete() {
-        t.Fatal("expecting incomplete key")
-    }
+	parentKey := nds.NewKey(c, "Parent", "", 1, nil)
+	incompleteKey := nds.NewIncompleteKey(c, "Test", parentKey)
+	if !incompleteKey.Incomplete() {
+		t.Fatal("expecting incomplete key")
+	}
 
-    if !incompleteKey.Parent().Equal(parentKey) {
-        t.Fatal("parent keys not equal")
-    }
+	if !incompleteKey.Parent().Equal(parentKey) {
+		t.Fatal("parent keys not equal")
+	}
 
-    key := NewKey(c, "Test", "string", 0, parentKey)
-    if key.Incomplete() {
-        t.Fatal("expected complete key")
-    }
+	key := nds.NewKey(c, "Test", "string", 0, parentKey)
+	if key.Incomplete() {
+		t.Fatal("expected complete key")
+	}
 
-    if !key.Parent().Equal(parentKey) {
-        t.Fatal("parent keys not equal")
-    }
+	if !key.Parent().Equal(parentKey) {
+		t.Fatal("parent keys not equal")
+	}
 }
 
 func TestEncodeDecode(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    key := NewKey(c, "Test", "string", 0, nil)
-    encVal := key.Encode()
+	key := nds.NewKey(c, "Test", "string", 0, nil)
+	encVal := key.Encode()
 
-    decKey, err := DecodeKey(encVal)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if !key.Equal(decKey) {
-        t.Fatal("key != decKey")
-    }
+	decKey, err := nds.DecodeKey(encVal)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !key.Equal(decKey) {
+		t.Fatal("key != decKey")
+	}
 }
 
 func TestGobEncodeDecode(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    key := NewKey(c, "Test", "string", 0, nil)
-    encVal, err := key.GobEncode()
-    if err != nil {
-        t.Fatal(err)
-    }
+	key := nds.NewKey(c, "Test", "string", 0, nil)
+	encVal, err := key.GobEncode()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    decKey := &Key{}
-    if err := decKey.GobDecode(encVal); err != nil {
-        t.Fatal(err)
-    }
+	decKey := &nds.Key{}
+	if err := decKey.GobDecode(encVal); err != nil {
+		t.Fatal(err)
+	}
 
-    if !key.Equal(decKey) {
-        t.Fatal("key != decKey")
-    }
+	if !key.Equal(decKey) {
+		t.Fatal("key != decKey")
+	}
 }
 
 func TestJSONMarshalUnmarshal(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    key := NewKey(c, "Test", "string", 0, nil)
-    encVal, err := key.MarshalJSON()
-    if err != nil {
-        t.Fatal(err)
-    }
+	key := nds.NewKey(c, "Test", "string", 0, nil)
+	encVal, err := key.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    decKey := &Key{}
-    if err := decKey.UnmarshalJSON(encVal); err != nil {
-        t.Fatal(err)
-    }
+	decKey := &nds.Key{}
+	if err := decKey.UnmarshalJSON(encVal); err != nil {
+		t.Fatal(err)
+	}
 
-    if !key.Equal(decKey) {
-        t.Fatal("key != decKey")
-    }
+	if !key.Equal(decKey) {
+		t.Fatal("key != decKey")
+	}
 }
 
 func TestAllocateIDs(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    parentKey := NewKey(c, "Parent", "string", 0, nil)
-    low, high, err := AllocateIDs(c, "Test", parentKey, 20)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if high - low != 20 {
-        t.Fatal("high - low != 20", high, low)
-    }
+	parentKey := nds.NewKey(c, "Parent", "string", 0, nil)
+	low, high, err := nds.AllocateIDs(c, "Test", parentKey, 20)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if high-low != 20 {
+		t.Fatal("high - low != 20", high, low)
+	}
 }
