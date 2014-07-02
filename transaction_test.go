a200961c692797b00fefec23a82ebea40b5dce25
Jonathan Gillham <jonathan.gillham@gamil.com>
2014-07-02 20:18:27 +0100

    Added transaction options.

---
+++ transaction_test.go
@@ -0,0 +1,51 @@
+package nds_test
+
+import (
+	"appengine"
+	"appengine/aetest"
+	"github.com/qedus/nds"
+	"testing"
+)
+
+func TestTransactionOptions(t *testing.T) {
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	type testEntity struct {
+		Val int
+	}
+
+	opts := &nds.TransactionOptions{XG: true}
+	err = nds.RunInTransaction(c, func(tc appengine.Context) error {
+		for i := 0; i < 4; i++ {
+			key := nds.NewIncompleteKey(tc, "Entity", nil)
+			if _, err := nds.Put(tc, key, &testEntity{i}); err != nil {
+				return err
+			}
+		}
+		return nil
+	}, opts)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	opts = &nds.TransactionOptions{XG: false}
+	err = nds.RunInTransaction(c, func(tc appengine.Context) error {
+		for i := 0; i < 4; i++ {
+			key := nds.NewIncompleteKey(tc, "Entity", nil)
+			if _, err := nds.Put(tc, key, &testEntity{i}); err != nil {
+				return err
+			}
+		}
+		return nil
+	}, opts)
+
+	if err == nil {
+		t.Fatal("expected cross-group error")
+	}
+
+}
