2f9ddec8ce328175c83a0e93bc5732a629b14714
Jonathan Gillham <jonathan.gillham@gamil.com>
2014-09-29 17:04:15 +0100

    Removed redundant markup.

--- doc.go
+++ doc.go
@@ -5,7 +5,7 @@ of making your app faster as memcache access is often 10x faster than datastore
 access. It can also make your app cheaper to run as memcache calls are free.
 
 This package goes to great lengths to ensure that stale datastore values are
-*never* returned to clients, i.e. the caching layer is strongly consistent. 
+never returned to clients, i.e. the caching layer is strongly consistent. 
 It does this by using a similar strategy to Python's ndb. However, this
 package fixes a couple of subtle edge case bugs that are found in ndb. See
 http://goo.gl/3ByVlA for one such bug.
@@ -15,8 +15,8 @@ employed by this package.
 
 Use
 
-Package nds is used *exactly* the same way as appeninge/datastore. Ensure that
-you change *all* your datastore Get, Put, Delete and RunInTransaction function
+Package nds is used exactly the same way as appeninge/datastore. Ensure that
+you change all your datastore Get, Put, Delete and RunInTransaction function
 calls to use nds when converting your own code.
 
 If you mix appengine/datastore and nds API calls then you are liable to get
