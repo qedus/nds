76c0495a230e14ca42caef6719ffed43e3157330
Jonathan Gillham <jonathan.gillham@gmail.com>
2014-10-27 18:45:02 +0000

    Consolidated memcache lock fail test.

--- get_test.go
+++ get_test.go
@@ -466,94 +466,6 @@ func TestGetMultiNonStruct(t *testing.T) {
 	}
 }
 
-// TestGetMemcacheFail makes sure that memcache failure does not stop Get from
-// working.
-func TestGetMemcacheFail(t *testing.T) {
-	c, err := aetest.NewContext(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer c.Close()
-
-	type testEntity struct {
-		IntVal int64
-	}
-
-	key := datastore.NewKey(c, "Entity", "", 1, nil)
-	val := &testEntity{3}
-
-	if _, err := nds.Put(c, key, val); err != nil {
-		t.Fatal(err)
-	}
-
-	nds.SetMemcacheAddMulti(func(c appengine.Context,
-		items []*memcache.Item) error {
-		return errors.New("expected memcache.AddMulti error")
-	})
-	nds.SetMemcacheCompareAndSwapMulti(func(c appengine.Context,
-		items []*memcache.Item) error {
-		return errors.New("expected memcache.ComapreAndSwap error")
-	})
-	nds.SetMemcacheGetMulti(func(c appengine.Context,
-		keys []string) (map[string]*memcache.Item, error) {
-		return nil, errors.New("expected memcache.GetMulti error")
-	})
-	nds.SetMemcacheSetMulti(func(c appengine.Context,
-		items []*memcache.Item) error {
-		return errors.New("expected memcache.SetMulti error")
-	})
-
-	defer func() {
-		nds.SetMemcacheAddMulti(nds.ZeroMemcacheAddMulti)
-		nds.SetMemcacheCompareAndSwapMulti(nds.ZeroMemcacheCompareAndSwapMulti)
-		nds.SetMemcacheGetMulti(nds.ZeroMemcacheGetMulti)
-		nds.SetMemcacheSetMulti(nds.ZeroMemcacheSetMulti)
-	}()
-
-	retVal := &testEntity{}
-	if err := nds.Get(c, key, retVal); err != nil {
-		t.Fatal(err)
-	}
-	if val.IntVal != retVal.IntVal {
-		t.Fatal("val and retVal not equal")
-	}
-}
-
-func TestGetMultiDatastoreFail(t *testing.T) {
-	c, err := aetest.NewContext(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer c.Close()
-
-	type testEntity struct {
-		IntVal int64
-	}
-
-	keys := []*datastore.Key{}
-	entities := []testEntity{}
-	for i := int64(1); i < 3; i++ {
-		keys = append(keys, datastore.NewKey(c, "Entity", "", i, nil))
-		entities = append(entities, testEntity{i})
-	}
-
-	if _, err := nds.PutMulti(c, keys, entities); err != nil {
-		t.Fatal(err)
-	}
-
-	nds.SetDatastoreGetMulti(func(c appengine.Context,
-		keys []*datastore.Key, vals interface{}) error {
-		return errors.New("expected datastore.GetMulti error")
-	})
-	defer nds.SetDatastoreGetMulti(datastore.GetMulti)
-
-	// Get from datastore.
-	response := make([]testEntity, len(keys))
-	if err := nds.GetMulti(c, keys, response); err == nil {
-		t.Fatal("expected GetMulti to fail")
-	}
-}
-
 func TestGetMultiMemcacheCorrupt(t *testing.T) {
 	c, err := aetest.NewContext(nil)
 	if err != nil {
@@ -653,57 +565,6 @@ func TestGetMultiMemcacheFlagCorrupt(t *testing.T) {
 	if err := nds.GetMulti(c, keys, response); err != nil {
 		t.Fatal(err)
 	}
-	for i := 0; i < len(keys); i++ {
-		if entities[i].IntVal != response[i].IntVal {
-			t.Fatal("IntVal not equal")
-		}
-	}
-}
-
-func errMemcacheGetMulti(c appengine.Context, keys []string) (
-	map[string]*memcache.Item, error) {
-
-	return nil, errors.New("test triggered memcache.GetMulti")
-}
-
-func TestGetMultiLockMemcacheFail(t *testing.T) {
-	c, err := aetest.NewContext(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer c.Close()
-
-	type testEntity struct {
-		IntVal int64
-	}
-
-	keys := []*datastore.Key{}
-	entities := []testEntity{}
-	for i := int64(1); i < 3; i++ {
-		keys = append(keys, datastore.NewKey(c, "Entity", "", i, nil))
-		entities = append(entities, testEntity{i})
-	}
-
-	if _, err := nds.PutMulti(c, keys, entities); err != nil {
-		t.Fatal(err)
-	}
-
-	memcacheGetChan := make(chan func(c appengine.Context, keys []string) (
-		map[string]*memcache.Item, error), 2)
-	memcacheGetChan <- nds.ZeroMemcacheGetMulti
-	memcacheGetChan <- errMemcacheGetMulti
-
-	nds.SetMemcacheGetMulti(func(c appengine.Context,
-		keys []string) (map[string]*memcache.Item, error) {
-		f := <-memcacheGetChan
-		return f(c, keys)
-	})
-	defer nds.SetMemcacheGetMulti(nds.ZeroMemcacheGetMulti)
-	response := make([]testEntity, len(keys))
-	if err := nds.GetMulti(c, keys, response); err != nil {
-		t.Fatal(err)
-	}
-
 	for i := 0; i < len(keys); i++ {
 		if entities[i].IntVal != response[i].IntVal {
 			t.Fatal("IntVal not equal")
@@ -1114,24 +975,47 @@ func TestGetMultiLockReturnMiss(t *testing.T) {
 }
 
 func TestGetMultiPaths(t *testing.T) {
+	expectedErr := errors.New("expected error")
 
 	type memcacheGetMultiFunc func(c appengine.Context,
 		keys []string) (map[string]*memcache.Item, error)
+	memcacheGetMultiFail := func(c appengine.Context,
+		keys []string) (map[string]*memcache.Item, error) {
+		return nil, expectedErr
+	}
 
 	type memcacheAddMultiFunc func(c appengine.Context,
 		items []*memcache.Item) error
+	memcacheAddMultiFail := func(c appengine.Context,
+		items []*memcache.Item) error {
+		return expectedErr
+	}
 
 	type memcacheCompareAndSwapMultiFunc func(c appengine.Context,
 		items []*memcache.Item) error
+	memcacheCompareAndSwapMultiFail := func(c appengine.Context,
+		items []*memcache.Item) error {
+		return expectedErr
+	}
 
 	type datastoreGetMultiFunc func(c appengine.Context,
 		keys []*datastore.Key, vals interface{}) error
+	datastoreGetMultiFail := func(c appengine.Context,
+		keys []*datastore.Key, vals interface{}) error {
+		return expectedErr
+	}
 
 	type marshalFunc func(pl datastore.PropertyList) ([]byte, error)
+	marshalFail := func(pl datastore.PropertyList) ([]byte, error) {
+		return nil, expectedErr
+	}
 
 	type unmarshalFunc func(data []byte, pl *datastore.PropertyList) error
-
-	expectedErr := errors.New("expected error")
+	/*
+	   unmarshalFail := func(data []byte, pl *datastore.PropertyList) error {
+	       return expectedErr
+	   }
+	*/
 
 	c, err := aetest.NewContext(nil)
 	if err != nil {
@@ -1157,23 +1041,30 @@ func TestGetMultiPaths(t *testing.T) {
 	tests := []struct {
 		description string
 
-		count int64
+		// Number of keys used to as GetMulti params.
+		keyCount int64
 
-		memcacheGetMultis           [2]memcacheGetMultiFunc
+		// Number of times GetMulti is called.
+		callCount int
+
+		// There are 2 memcacheGetMulti calls for every GetMulti call.
+		memcacheGetMultis           []memcacheGetMultiFunc
 		memcacheAddMulti            memcacheAddMultiFunc
 		memcacheCompareAndSwapMulti memcacheCompareAndSwapMultiFunc
 
 		datastoreGetMulti datastoreGetMultiFunc
 
-		marshal    marshalFunc
-		unmarshals [2]unmarshalFunc
+		marshal marshalFunc
+		// There are 2 unmarshal calls for every GetMultiCall.
+		unmarshals []unmarshalFunc
 
-		expectedErr error
+		expectedErrs []error
 	}{
 		{
 			"no errors",
 			20,
-			[2]memcacheGetMultiFunc{
+			1,
+			[]memcacheGetMultiFunc{
 				nds.ZeroMemcacheGetMulti,
 				nds.ZeroMemcacheGetMulti,
 			},
@@ -1181,16 +1072,35 @@ func TestGetMultiPaths(t *testing.T) {
 			nds.ZeroMemcacheCompareAndSwapMulti,
 			datastore.GetMulti,
 			nds.MarshalPropertyList,
-			[2]unmarshalFunc{
+			[]unmarshalFunc{
 				nds.UnmarshalPropertyList,
 				nds.UnmarshalPropertyList,
 			},
-			nil,
+			[]error{nil},
+		},
+		{
+			"datastore unknown error",
+			2,
+			1,
+			[]memcacheGetMultiFunc{
+				nds.ZeroMemcacheGetMulti,
+				nds.ZeroMemcacheGetMulti,
+			},
+			nds.ZeroMemcacheAddMulti,
+			nds.ZeroMemcacheCompareAndSwapMulti,
+			datastoreGetMultiFail,
+			nds.MarshalPropertyList,
+			[]unmarshalFunc{
+				nds.UnmarshalPropertyList,
+				nds.UnmarshalPropertyList,
+			},
+			[]error{expectedErr},
 		},
 		{
 			"datastore unknown multierror",
 			2,
-			[2]memcacheGetMultiFunc{
+			1,
+			[]memcacheGetMultiFunc{
 				nds.ZeroMemcacheGetMulti,
 				nds.ZeroMemcacheGetMulti,
 			},
@@ -1206,37 +1116,74 @@ func TestGetMultiPaths(t *testing.T) {
 				return me
 			},
 			nds.MarshalPropertyList,
-			[2]unmarshalFunc{
+			[]unmarshalFunc{
 				nds.UnmarshalPropertyList,
 				nds.UnmarshalPropertyList,
 			},
-			appengine.MultiError{expectedErr, expectedErr},
+			[]error{
+				appengine.MultiError{expectedErr, expectedErr},
+			},
 		},
 		{
 			"marshal error",
 			5,
-			[2]memcacheGetMultiFunc{
+			1,
+			[]memcacheGetMultiFunc{
 				nds.ZeroMemcacheGetMulti,
 				nds.ZeroMemcacheGetMulti,
 			},
 			nds.ZeroMemcacheAddMulti,
 			nds.ZeroMemcacheCompareAndSwapMulti,
 			datastore.GetMulti,
-			func(pl datastore.PropertyList) ([]byte, error) {
-				return nil, expectedErr
-			},
-			[2]unmarshalFunc{
+			marshalFail,
+			[]unmarshalFunc{
 				nds.UnmarshalPropertyList,
 				nds.UnmarshalPropertyList,
 			},
-			nil,
+			[]error{nil},
+		},
+		{
+			"total memcache fail",
+			20,
+			1,
+			[]memcacheGetMultiFunc{
+				memcacheGetMultiFail,
+				memcacheGetMultiFail,
+			},
+			memcacheAddMultiFail,
+			memcacheCompareAndSwapMultiFail,
+			datastore.GetMulti,
+			nds.MarshalPropertyList,
+			[]unmarshalFunc{
+				nds.UnmarshalPropertyList,
+				nds.UnmarshalPropertyList,
+			},
+			[]error{nil},
+		},
+		{
+			"lock memcache fail",
+			20,
+			1,
+			[]memcacheGetMultiFunc{
+				nds.ZeroMemcacheGetMulti,
+				memcacheGetMultiFail,
+			},
+			nds.ZeroMemcacheAddMulti,
+			nds.ZeroMemcacheCompareAndSwapMulti,
+			datastore.GetMulti,
+			nds.MarshalPropertyList,
+			[]unmarshalFunc{
+				nds.UnmarshalPropertyList,
+				nds.UnmarshalPropertyList,
+			},
+			[]error{nil},
 		},
 	}
 
 	for _, test := range tests {
 		t.Log("Start", test.description)
 
-		keys, putVals := keysVals(c, test.count)
+		keys, putVals := keysVals(c, test.keyCount)
 		if _, err := nds.PutMulti(c, keys, putVals); err != nil {
 			t.Fatal(err)
 		}
@@ -1273,8 +1220,47 @@ func TestGetMultiPaths(t *testing.T) {
 			return fn(data, pl)
 		})
 
-		getVals := make([]testEntity, test.count)
-		err := nds.GetMulti(c, keys, getVals)
+		for i := 0; i < test.callCount; i++ {
+			getVals := make([]testEntity, test.keyCount)
+			err := nds.GetMulti(c, keys, getVals)
+
+			expectedErr := test.expectedErrs[i]
+
+			if expectedErr == nil {
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				for i := range getVals {
+					if getVals[i].IntVal != putVals[i].IntVal {
+						t.Fatal("incorrect IntVal")
+					}
+				}
+			} else {
+				if err == nil {
+					t.Fatal("expected error")
+				}
+				expectedMultiErr, isMultiErr := expectedErr.(appengine.MultiError)
+
+				if isMultiErr {
+					me, ok := err.(appengine.MultiError)
+					if !ok {
+						t.Fatal("expected appengine.MultiError but got", err)
+					}
+
+					if len(me) != len(expectedMultiErr) {
+						t.Fatal("appengine.MultiError length incorrect")
+					}
+
+					for i, e := range me {
+						if e != expectedMultiErr[i] {
+							t.Fatal("non matching errors",
+								e, expectedMultiErr[i])
+						}
+					}
+				}
+			}
+		}
 
 		// Reset App Engine API calls.
 		nds.SetMemcacheGetMulti(nds.ZeroMemcacheGetMulti)
@@ -1283,40 +1269,6 @@ func TestGetMultiPaths(t *testing.T) {
 		nds.SetDatastoreGetMulti(datastore.GetMulti)
 		nds.SetMarshal(nds.MarshalPropertyList)
 		nds.SetUnmarshal(nds.UnmarshalPropertyList)
-
-		if test.expectedErr == nil {
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			for i := range getVals {
-				if getVals[i].IntVal != putVals[i].IntVal {
-					t.Fatal("incorrect IntVal")
-				}
-			}
-		} else {
-			if err == nil {
-				t.Fatal("expected error")
-			}
-			expectedMultiErr, isMultiErr := test.expectedErr.(appengine.MultiError)
-
-			if isMultiErr {
-				me, ok := err.(appengine.MultiError)
-				if !ok {
-					t.Fatal("expected appengine.MultiError but got", err)
-				}
-
-				if len(me) != len(expectedMultiErr) {
-					t.Fatal("appengine.MultiError length incorrect")
-				}
-
-				for i, e := range me {
-					if e != expectedMultiErr[i] {
-						t.Fatal("non matching errors", e, expectedMultiErr[i])
-					}
-				}
-			}
-		}
 
 		if err := nds.DeleteMulti(c, keys); err != nil {
 			t.Fatal(err)
