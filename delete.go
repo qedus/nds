65eb4a028156a32326d04e43d20917db229e55eb
Jonathan Gillham <jonathan.gillham@gmail.com>
2014-11-17 14:00:12 +0000

    Deferred memcache locking for delete in transaction.

--- delete.go
+++ delete.go
@@ -41,7 +41,10 @@ func deleteMulti(c appengine.Context, keys []*datastore.Key) error {
 	}
 
 	// Make sure we can lock memcache with no errors before deleting.
-	if err := memcacheSetMulti(c, lockMemcacheItems); err != nil {
+	if txc, ok := c.(txContext); ok {
+		txc.lockMemcacheItems = append(txc.lockMemcacheItems,
+			lockMemcacheItems...)
+	} else if err := memcacheSetMulti(c, lockMemcacheItems); err != nil {
 		return err
 	}
 
