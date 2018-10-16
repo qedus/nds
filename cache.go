2ee4597732f2b8433f9c182f03e61ea7af8f4804
Prateek Malhotra <prateek@condorcapital.com>
2018-10-16 18:19:12 -0400

    Add doc comments to Cacher interface

--- cache.go
+++ cache.go
@@ -43,13 +43,33 @@ var (
 	ErrNotStored = errors.New("nds: not stored")
 )
 
+// Cacher represents a cache backend that can be used by nds.
 type Cacher interface {
+	// NewContext returns the same or a derivative of the passed in context. The returned context will
+	// be used in subsequent calls to the other Cacher functions.
 	NewContext(c context.Context) (context.Context, error)
+	// AddMulti adds each provided Item into the cache if and only if the key for the item is not
+	// currently in use. For any item that was not stored due to a key conflict, a MultiError is returned
+	// with ErrNotStored in the corresponding index for that item. There may be other errors for each MultiError index.
 	AddMulti(c context.Context, items []*Item) error
+	// CompareAndSwapMulti will set each item in the cache if the item was unchanged since it was last got.
+	// The items are modified versions of items returned by the GetMulti call for this Cacher.
+	// If any item was not able to be stored, a MultiError will be returned with ErrCASConflict if the value in
+	// cache was changed since the GetMulti call, or ErrNotStored if the key was evicted or could otherwise not be
+	// stored in the corresponding index for that item. There may be other errors for each MultiError index.
 	CompareAndSwapMulti(c context.Context, items []*Item) error
-	DeleteMulti(c context.Context, key []string) error
-	GetMulti(c context.Context, key []string) (map[string]*Item, error)
-	SetMulti(c context.Context, item []*Item) error
+	// DeleteMulti deletes all keys provided in the cache. It is not necessary to track individual key misses though
+	// the proper way would be to return a MultiError with ErrCacheMiss stored in the corresponding index for that key.
+	DeleteMulti(c context.Context, keys []string) error
+	// GetMulti fetches all provided keys from the cache, returning a map[string]*Item filled with all Items
+	// it was able to retrieve, using the item key as the lookup key. The cache implementation should utilize the SetCAS/
+	// GetCAS info calls of each Item as required so subsequent calls to CompareAndSwapMulti can correctly execute. No
+	// error should be returned for any key misses.
+	GetMulti(c context.Context, keys []string) (map[string]*Item, error)
+	// SetMulti sets all provided items in the cache, regardless of whether or not the key is already in use and the
+	// value of that stored item. If any item could not be stored A MultiError should be returned with an error in the
+	// corresponding index for that item. Note: ErrNotStored is not a valid error to be used for any error returned.
+	SetMulti(c context.Context, items []*Item) error
 }
 
 // Item is the unit of Cacher gets and sets.
