fa3611f2dba078aa4c4eea8ecaf7afbd614bc2e5
Prateek Malhotra <prateek@condorcapital.com>
2019-01-16 10:46:18 -0500

    Removed Config and updated options to work directly on Client

--- client.go
+++ client.go
@@ -13,34 +13,28 @@ type Client struct {
 	cacher    Cacher
 	onErrorFn OnErrorFunc
 
+	// TODO: Client is exported since we embedded datastore.Client - fix this
 	*datastore.Client
 }
 
 // ClientOption is an option for a nds Client.
 // Inspired by Google's api/option package.
-type ClientOption func(*Config)
-
-type Config struct {
-	// DatatstoreClient is the prepared client to use for interacting with
-	// Google Cloud Datastore with.
-	datastoreClient *datastore.Client
-	// OnError is called for every internal error that doesn't return to the
-	// caller but maybe useful to capture for logging/debugging/reporting
-	// purposes. For example, to keep the original nds.v1 behavior of logging
-	// warnings in AppEngine Standard, this could be:
-	// func(ctx context.Context, err error) { log.Warningf(ctx, "%s", err) }
-	// By default this will log the error using the standard go log package.
-	onErrorFn OnErrorFunc
-}
+type ClientOption func(*Client)
 
 func WithDatastoreClient(ds *datastore.Client) ClientOption {
-	return func(c *Config) {
-		c.datastoreClient = ds
+	return func(c *Client) {
+		c.Client = ds
 	}
 }
 
+// WithOnErrorFunc sets up an OnErrorFunc to be called for every internal
+// error that doesn't return to the caller but maybe useful to capture for
+// logging/debugging/reporting purposes. For example, to keep the original
+// nds.v1 behavior of logging warnings in AppEngine Standard, this could be:
+// func(ctx context.Context, err error) { log.Warningf(ctx, "%s", err) }
+// By default this will log the error using the standard go log package.
 func WithOnErrorFunc(f OnErrorFunc) ClientOption {
-	return func(c *Config) {
+	return func(c *Client) {
 		c.onErrorFn = f
 	}
 }
@@ -48,26 +42,24 @@ func WithOnErrorFunc(f OnErrorFunc) ClientOption {
 // NewClient will return an nds.Client that can be used exactly like a datastore.Client but will
 // transparently use the cache configuration provided to cache requests when it can.
 func NewClient(ctx context.Context, cacher Cacher, opts ...ClientOption) (*Client, error) {
-	var cfg Config
-
-	for _, opt := range opts {
-		opt(&cfg)
+	client := &Client{
+		cacher: cacher,
 	}
 
-	if cfg.datastoreClient == nil {
+	for _, opt := range opts {
+		opt(client)
+	}
+
+	if client.Client == nil {
 		// Default datastore.Client
 		if ds, err := datastore.NewClient(ctx, ""); err != nil {
 			return nil, err
 		} else {
-			cfg.datastoreClient = ds
+			client.Client = ds
 		}
 	}
 
-	return &Client{
-		cacher:    cacher,
-		onErrorFn: cfg.onErrorFn,
-		Client:    cfg.datastoreClient,
-	}, nil
+	return client, nil
 }
 
 func (c *Client) onError(ctx context.Context, err error) {
